7a933387f3e144c6813fd47a40e1512e2a6553b3
Ilmitico9676 <65183352+Ilmitico9676@users.noreply.github.com>
2021-11-14 22:04:43 +0100

    main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import pyautogui
+from time import sleep
+sleep (10)
+for numero in range(0,1000):
+    pyautogui.click(1246, 980)
+    sleep(0.50)
+    pyautogui.click(1503,661)
+    sleep(0.50)
+    pyautogui.click(1024,590)
+    sleep(0.50)
+    pyautogui.click(130, 81)
+    sleep(0.50)
